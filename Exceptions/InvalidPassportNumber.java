37073547c8bfd94b1dbd92025833ca8ddd245549
carson99 <44386229+carson99@users.noreply.github.com>
2021-03-08 09:42:22 -0500

    Add files via upload
    Exceptions assignment

---
+++ Exceptions/InvalidPassportNumber.java
@@ -0,0 +1,13 @@
+package fourClass;
+
+public class InvalidPassportNumber extends Exception {
+	
+	public InvalidPassportNumber() {
+		super();
+	}
+	public InvalidPassportNumber(final String message) {
+		super(message);
+		
+	}
+
+}
